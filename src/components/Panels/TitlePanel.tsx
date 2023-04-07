7199a695c52ab3bc8b4878a4316a2948f45d9ba1
Iain Black <57415656+iainblack@users.noreply.github.com>
2023-04-07 02:43:13 -0700

    add some animations

--- src/components/Panels/TitlePanel.tsx
+++ src/components/Panels/TitlePanel.tsx
@@ -5,6 +5,7 @@ import {
   Slide,
   Typography,
   Button,
+  Fade,
 } from "@mui/material";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
@@ -54,52 +55,60 @@ export default function TitlePanel(props: TitlePanelProps) {
           }}
         >
           <Box sx={{ maxWidth: 500, mt: { xs: 12, md: 6 } }}>
-            <Typography
-              variant={isSmallScreen ? "h4" : "h3"}
-              fontFamily="header"
-              sx={{ color: "white", mb: 1 }}
-            >
-              {" Lorem ipsum dolor"}
-            </Typography>
-            <Typography fontFamily="body" sx={{ color: "white" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse ut risus convallis, aliquet felis sit amet, vulputate
-              tellus. Sed mattis quis nunc eu maximus.
-            </Typography>
+            <Fade in timeout={1000}>
+              <Typography
+                variant={isSmallScreen ? "h4" : "h3"}
+                fontFamily="header"
+                sx={{ color: "white", mb: 1 }}
+              >
+                {"Pizza, the right way."}
+              </Typography>
+            </Fade>
+            <Fade in timeout={1500}>
+              <Typography fontFamily="body" sx={{ color: "white" }}>
+                At Benniditos we have have been feeding Spokane since 19XX
+                adipiscing elit. Suspendisse ut risus convallis, aliquet felis
+                sit amet, vulputate tellus. Sed mattis quis nunc eu maximus.
+              </Typography>
+            </Fade>
           </Box>
           <Box sx={{ mt: 6 }}>
-            <Box sx={{ mb: 2 }}>
-              <Typography sx={{ color: "white", fontFamily: "header" }}>
-                Benniditos Pizza
-              </Typography>
-              <Typography sx={{ color: "white", fontFamily: "header" }}>
-                South Hill, Spokane, WA
-              </Typography>
-              <Button
-                sx={{ textTransform: "none", fontFamily: "body" }}
-                onClick={() => {
-                  window.open("tel:+5094557411", "_blank");
-                }}
-              >
-                509-455-7411
-              </Button>
-            </Box>
-            <Box>
-              <Typography sx={{ color: "white", fontFamily: "header" }}>
-                Benniditos BrewPub
-              </Typography>
-              <Typography sx={{ color: "white", fontFamily: "header" }}>
-                East on Sprague, Spokane, WA
-              </Typography>
-              <Button
-                sx={{ textTransform: "none", fontFamily: "body" }}
-                onClick={() => {
-                  window.open("tel:+5092905018", "_blank");
-                }}
-              >
-                509-290-5018
-              </Button>
-            </Box>
+            <Fade in timeout={2000}>
+              <Box sx={{ mb: 2 }}>
+                <Typography sx={{ color: "white", fontFamily: "header" }}>
+                  Benniditos Pizza
+                </Typography>
+                <Typography sx={{ color: "white", fontFamily: "header" }}>
+                  South Hill, Spokane, WA
+                </Typography>
+                <Button
+                  sx={{ textTransform: "none", fontFamily: "body" }}
+                  onClick={() => {
+                    window.open("tel:+5094557411", "_blank");
+                  }}
+                >
+                  509-455-7411
+                </Button>
+              </Box>
+            </Fade>
+            <Fade in timeout={2500}>
+              <Box>
+                <Typography sx={{ color: "white", fontFamily: "header" }}>
+                  Benniditos BrewPub
+                </Typography>
+                <Typography sx={{ color: "white", fontFamily: "header" }}>
+                  East on Sprague, Spokane, WA
+                </Typography>
+                <Button
+                  sx={{ textTransform: "none", fontFamily: "body" }}
+                  onClick={() => {
+                    window.open("tel:+5092905018", "_blank");
+                  }}
+                >
+                  509-290-5018
+                </Button>
+              </Box>
+            </Fade>
           </Box>
         </Box>
       </Box>
