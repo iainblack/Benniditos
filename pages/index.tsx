3a8f2adef567e2770d2a7f0cd02b8ce45ff7ff8f
Iain Black <57415656+iainblack@users.noreply.github.com>
2023-05-01 15:46:47 -0700

    add viewport meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -277,6 +277,8 @@ export default function BenniditosHome() {
       <CssBaseline />
       <Head>
         <title>Bennidito&apos;s</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box
         id="benniditosHome"
